e66a879e29cc46746022945b7ebe6889b80e6d3c
Lorenz Schenkel (PT-331) <Lorenz.Schenkel@bmw.de>
2020-04-24 09:34:35 +0200

    Comments and start of gausian filter but don not get numpy array so i try another methode for tf record
    Signed-off-by: Lorenz Schenkel (PT-331) <Lorenz.Schenkel@bmw.de>

--- task_run_cnn.py
+++ task_run_cnn.py
@@ -3,7 +3,9 @@ import os
 import IPython.display as display
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 import matplotlib.image as mpimg
+
 
 
 # set gpu no for traning
@@ -35,11 +37,10 @@ def main(_):
         for i in range(model.N_SAMPLES):                            # model.N_Samples = 3
             x_val,y_val = sess.run([x,y])
 
-            #Task 3:
+            #TODO:Task 3:
             # - Apply Gausian filter to image of Homer
             # - Save image
             # - Push snapshot and image to GIT
-            #...
 
         # saver
         tf.train.save(sess, 'my_test_model')                        # save the training from ki
@@ -48,7 +49,7 @@ def main(_):
         coord.join(threads)                                         # wait for the threads to terminate
         print('done')
 
-class CNN():                                                        # Model
+class CNN():                                                          # Model
     def __init__(self):                                             # initialise vaiables
         self.N_SAMPLES = 3
         self.DATASET = ...
@@ -58,13 +59,12 @@ class CNN():                                                        # Model
 
 # read indiviual images
     def getInput(self):
-        # Task 1:
-        # Read TfRecord
         print("in CNN getInput()")
 
-        filename = 'C:\\Users\\Q447230\\Coden\\gitRepositorys\\Coding_Task\\128px-Felis_catus-cat_on_snow.jpg' #'128px-Felis_catus-cat_on_snow.jpg',
+        # TFRecodDataset(filename=tf.string, tf.data.Dataset)
         dataset = tf.data.TFRecordDataset("C:\\Users\\Q447230\\Coden\\gitRepositorys\\Coding_Task\\record_test.tfrecord")
 
+        # dict datanames, types what we expect to see in the tf.recod file | expect what is in the file ang take it
         features = {
             'img': tf.io.FixedLenFeature([], tf.string),
             'x': tf.io.FixedLenFeature([], tf.int64),
@@ -72,48 +72,54 @@ class CNN():                                                        # Model
             'z': tf.io.FixedLenFeature([], tf.int64),
         }
 
-        def _parse_image_function(example_proto):
-            # Parse the input tf.Example proto using the dictionary above.
-            return tf.io.parse_single_example(example_proto, features)
+        def _parse_image_function(example):
+            #tf.io.parse_single_example(idk; features = dict with feature keys) | get a dict with our data in raw bytes
+            return tf.io.parse_single_example(example, features)
 
+        # transform the whole dataset and return the new one
         image_dataset = dataset.map(_parse_image_function)
 
+        # illeiterate about the dataset (there are 3 images)
         for image_features in image_dataset:
+            #  converted to NumPy ndarraysv
             image_raw = image_features['img'].numpy()
-            display.display(display.Image(data=image_raw))
-
+            print("image_raw", image_raw)
+            image_raw = image_features['img'].numpy()
+            print("image_raw", image_raw)
+            #display.display(display.Image(data=image_raw))
+            CNN.inference(self, img=image_raw)
 
     def inference(self, img):
-        with tf.name_scope('conv1'):
-            out = 1
-            k_w = 5
-            k_h = 5
-            s = [1, 1, 1, 1]
-            pad = 'VALID'
-            w_shape = [k_w, k_h, 3, out]
+        print("in inference()")
 
-# Stackoverflow
-            # def gaussian_kernel(size: int,
-            #                     mean: float,
-            #                     std: float,
-            #                     ):
-            #     """Makes 2D gaussian Kernel for convolution."""
-            #
-            #     d = tf.distributions.Normal(mean, std)
-            #
-            #     vals = d.prob(tf.range(start=5, limit=5, dtype=tf.float32))
-            #
-            #     gauss_kernel = tf.einsum('i,j->ij',
-            #                              vals,
-            #                              vals)
+        # with tf.name_scope('conv1'):
+        #     out = 1
+        #     k_w = 5
+        #     k_h = 5
+        #     s = [1, 1, 1, 1]
+        #     pad = 'VALID'
+        #     w_shape = [k_w, k_h, 3, out]
+        #TODO: Task 2
+        # - Apply a 5x5 Gausian filter to the input
 
-            #Task 2
-            # - Apply a 5x5 Gausian filter to the input
-            #...
+        #returns a new array of given shape and datatype, where the element's value is set to 1
+        # np.ones(shape = define size of array, dtype = spezify the data
+        kernel = np.ones((5, 5), np.float32) / 25
+        print("kernel", kernel)
+        print("img", img)
 
-            #h_conv1 = tf.nn.conv2d(img, w_conv1, strides=s, padding=pad, name='h_conv1')
-            #h_conv1 = b_conv1*h_conv1
-            #h_conv1
+        # need as src numpy array
+        dst = cv2.filter2D(img, -1, kernel)
+        blur = cv2.GaussianBlur(img, (5, 5), 0)
+
+        plt.subplot(121), plt.imshow(img), plt.title('Original')
+        plt.xticks([]), plt.yticks([])
+        plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
+        plt.xticks([]), plt.yticks([])
+        plt.show()
+
+
+
         return
 
 #if __name__ == '__main__':
