4c1a8447db3fa9a20782c69edd51bcffc847a737
Lorenz Schenkel (PT-331) <Lorenz.Schenkel@bmw.de>
2020-06-03 10:10:33 +0200

    I could picture the image. Trying to set a self made (https://dmyhaspl.github.io/2018/10/20/Gaussian-blur-2018/) filter in the inteference mode. I expand it to 5 kernels but i have problems with the shape and don't know how to fix it. Now trying to get a real tensor from the methode and attempt to do it with cv2

--- task_run_cnn.py
+++ task_run_cnn.py
@@ -1,3 +1,5 @@
+import math
+
 import tensorflow as tf
 import os
 import IPython.display as display
@@ -8,6 +10,8 @@ import cv2
 
 
 # set gpu no for traning
+from PIL.Image import Image
+
 os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
 
 #Complete code at ...
@@ -34,7 +38,7 @@ def main(_):
         tf.train.start_queue_runners(sess=sess)                     # start threads and give a list with threads back
 
         for i in range(model.N_SAMPLES):                            # model.N_Samples = 3
-            x_val,y_val = sess.run([x,y])
+            x_val, y_val = sess.run([x, y])
 
             #Task 3:
             # - Apply Gausian filter to image of Homer
@@ -74,10 +78,11 @@ class CNN():                                                        # Model
 
             parsed_example = tf.parse_single_example(serialized=serialized, features=features)
 
-
             image_raw = parsed_example["img"]
             image = tf.decode_raw(image_raw, tf.uint8)
-            image = tf.cast(image, tf.float32)
+
+            # only float if we want a tensor
+            #image = tf.cast(image, tf.float32)
 
             return image
 
@@ -86,68 +91,253 @@ class CNN():                                                        # Model
         dataset = self.DATASET.map(_parse_image_function)
 
         #dataset = dataset.repeat(1)
-        # dataset = image_features.repeat(1)
 
         dataset = dataset.batch(3)
-        # dataset = image_features.batch(3)
 
         iterator = dataset.make_one_shot_iterator()
-        # iterator = image_features.make_one_shot_iterator()
 
-        images_batch = iterator.get_next()
+        tensor = iterator.get_next()
+        print("tensor", tensor)
 
         #x = {"img": images_batch}
 
-        y = images_batch.eval(session=tf.compat.v1.Session())
+        rawBytesArray = tensor.eval(session=tf.compat.v1.Session())
 
 
-        for tensor in y:
+        for oneRawBytesArray in rawBytesArray:
 
-            print("type", type(tensor))
+            print("oneTensor", oneRawBytesArray)
+            # print("tensor", rawBytesArray)
+
+            oneRawBytesArray = oneRawBytesArray.reshape((303, 303, 3))
+            #oneRawBytesArray = np.reshape(a=oneRawBytesArray, newshape=[303, 303, 3])
+
+            # blur = cv2.GaussianBlur(oneRawBytesArray, (5, 5), 0)
+            #
+            # plt.subplot(121), plt.imshow(oneRawBytesArray), plt.title('without filter')
+            # plt.xticks([]), plt.yticks([])
+            # plt.subplot(122), plt.imshow(blur), plt.title('with filter')
+            # plt.xticks([]), plt.yticks([])
+            # plt.show()
+
+            CNN.inference(self, tensor, oneRawBytesArray, rawBytesArray)
+        return
 
 
-            #tensor = bytearray(tensor)
+    def inference(self, tensor, oneRawBytesArray, rawBytesArray):
 
-            print("type", type(tensor))
+        # Gausian filter formula()
+        def getGuessValue(kerStd, posX, posY):
+            return 1. / (2. * math.pi * (np.power(kerStd, 2))) * math.exp(
+                -(np.power(posX, 2) + np.power(posY, 2)) / (2. * (np.power(kerStd, 2))))
 
+        def getGuessKernel(kerStd):
+            K00 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, -1, 1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K10 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 0, 1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K20 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, 1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K30 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, -1, 0), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K40 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 0, 0), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K01 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, 0), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K11 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, -1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K21 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 0, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K31 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K41 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K02 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K12 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K22 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K32 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K42 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K03 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K13 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K23 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K33 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K43 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K04 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K14 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K24 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K34 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            K44 = np.column_stack(
+                (np.row_stack((np.eye(3) * getGuessValue(kerStd, 1, -1), [0., 0., 0.])), np.array([0., 0., 0., 1.])))
+            print(K11.shape)
+            kernel = tf.constant(np.array(
+                [
+                    [
+                        K00,
+                        K10,
+                        K20,
+                        K30,
+                        K40
+                    ],
+                    [
+                        K01,
+                        K11,
+                        K21,
+                        K31,
+                        K41
+                    ],
+                    [
+                        K02,
+                        K12,
+                        K22,
+                        K32,
+                        K42
+                    ],
+                    [
+                        K03,
+                        K13,
+                        K23,
+                        K33,
+                        K43
+                    ],
+                    [
+                        K04,
+                        K14,
+                        K24,
+                        K34,
+                        K44
+                    ]
+                ])
+                , dtype=tf.float32)  # 3*3*4*4 | 5,5,4,4
+            return kernel
 
-            tensor = np.reshape(tensor, (303, 303, 3))
+        def getImageData(oneRawBytesArray):
+            return np.array(oneRawBytesArray, dtype=np.float32)
 
-            # tf.reshape(tensor, (303, 303, 3))
+        imageData = getImageData(oneRawBytesArray)
+        testData = tf.constant(imageData)
+        kernel = getGuessKernel(0.8)
+        y = tf.cast(tf.nn.conv2d(testData, kernel, strides=[5, 5, 4, 4], padding="SAME"), dtype=tf.int32)
+        init_op = tf.global_variables_initializer()
 
-            print(tensor)
-
-            blur = cv2.GaussianBlur(tensor, (5, 5), 0)
-
-            plt.subplot(121), plt.imshow(tensor), plt.title('without filter')
-            plt.xticks([]), plt.yticks([])
-            plt.subplot(122), plt.imshow(blur), plt.title('with filter')
-            plt.xticks([]), plt.yticks([])
-            plt.show()
-
-            print(tensor)
+        g = tf.Graph()
+        with tf.Session(graph=g) as sess:
+            # print(testData.get_shape())
+            # print(kernel.eval())
+            # print(kernel.get_shape())
+            resultData = sess.run(y)[0]
+            print(resultData.shape)
+            resulImage = Image.fromarray(np.uint8(resultData))
+            resulImage.show()
+            print(y.get_shape())
 
         return y
 
 
-    def inference(self, img):
-        with tf.name_scope('conv1'):
-            out = 1
-            k_w = 5
-            k_h = 5
-            s = [1, 1, 1, 1]
-            pad = 'VALID'
-            w_shape = [k_w, k_h, 3, out]
 
-            g = tf.Graph()
-            with g.as_default():
-                pass
 
-            #Task 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # with tf.name_scope('conv1'):
+        #     out = 1
+            # k_w = 5
+            # k_h = 5
+            # s = [1, 1, 1, 1]
+            # pad = 'VALID'
+            # w_shape = [k_w, k_h, 3, out]
+            #
+            # print("oneRawBytesArray:", oneRawBytesArray)
+            # np.array(oneRawBytesArray)
+            #
+            #
+            # a = tf.constant(oneRawBytesArray)
+            # b = cv2.GaussianBlur(a, (5, 5), 0)
+            # print("a", a)
+            #
+            # print("before Session")
+            # sess = tf.compat.v1.Session()
+            # print(sess.run(b))
+
+            # g = tf.Graph()
+            # with g.as_default():
+            #     pass
+
+            # def gaussian_blur(img, kernel_size=11, sigma=5):
+            #     def gauss_kernel(channels, kernel_size, sigma):
+            #         ax = tf.range(-kernel_size // 2 + 1.0, kernel_size // 2 + 1.0)
+            #         xx, yy = tf.meshgrid(ax, ax)
+            #         kernel = tf.exp(-(xx ** 2 + yy ** 2) / (2.0 * sigma ** 2))
+            #         kernel = kernel / tf.reduce_sum(kernel)
+            #         kernel = tf.tile(kernel[..., tf.newaxis], [1, 1, channels])
+            #         return kernel
+            #
+            #     gaussian_kernel = gauss_kernel(tf.shape(img)[-1], kernel_size, sigma)
+            #     gaussian_kernel = gaussian_kernel[..., tf.newaxis]
+            #
+            #     return tf.nn.depthwise_conv2d(img, gaussian_kernel, [1, 1, 1, 1], padding='SAME', data_format='NHWC')
+
+            #TODO: Task 2
             # - Apply a 5x5 Gausian filter to the input
-            #...
+            # - with graph and session
 
-        return
+
 
 #if __name__ == '__main__':
     #tf.app.run()
